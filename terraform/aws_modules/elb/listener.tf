dd28969056046c9f100d5305604153f8ff83e5f2
silasfrancis <silasfrancis07@gmail.com>
2026-03-10 18:18:52 +0100

    application security checks

--- terraform/aws_modules/elb/listener.tf
+++ terraform/aws_modules/elb/listener.tf
@@ -18,7 +18,7 @@ resource "aws_lb_listener" "https" {
   port              = 443
   protocol          = "HTTPS"
 
-  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
+  ssl_policy      = "ELBSecurityPolicy-TLS-1-2-2017-01"
   certificate_arn = var.certificate_arn
 
   default_action {
