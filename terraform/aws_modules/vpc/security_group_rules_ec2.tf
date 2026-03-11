adb97362fcd4d818dc2dc3c14a7c5841ad8e3973
silasfrancis <silasfrancis07@gmail.com>
2026-03-11 10:07:31 +0100

    deployment

--- terraform/aws_modules/vpc/security_group_rules_ec2.tf
+++ terraform/aws_modules/vpc/security_group_rules_ec2.tf
@@ -1,8 +1,20 @@
-resource "aws_vpc_security_group_ingress_rule" "ec2_ingress_rule_app" {
-  description = "Allow inbound traffic from ALB to EC2 on port 3000"  
+resource "aws_vpc_security_group_ingress_rule" "ec2_ingress_rule_app_blue" {
+  description = "Allow inbound traffic from ALB to EC2 on port 3000 for blue deployments"  
   security_group_id = aws_security_group.ec2.id
   from_port = 3000
   to_port = 3000
+  ip_protocol = "tcp"
+  referenced_security_group_id = aws_security_group.alb.id
+  tags = {
+  Resource = "EC2"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_ingress_rule_app_green" {
+  description = "Allow inbound traffic from ALB to EC2 on port 3001 for green deployments"  
+  security_group_id = aws_security_group.ec2.id
+  from_port = 3001
+  to_port = 3001
   ip_protocol = "tcp"
   referenced_security_group_id = aws_security_group.alb.id
   tags = {
